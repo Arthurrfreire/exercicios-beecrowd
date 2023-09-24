0dc366fed49d690fdb3d10ab9bc08bca63cbd162
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2023-09-24 11:31:29 -0300

    foi

--- exercicios-bc/1016.py
+++ exercicios-bc/1016.py
@@ -1,12 +1,22 @@
-#Dois carros (X e Y) partem em uma mesma direção. O carro X sai com velocidade constante de 60 Km/h e o carro Y sai com velocidade constante de 90 Km/h.
+'''Dois carros (X e Y) partem em uma mesma direção. O carro X sai com velocidade constante de 60 Km/h e o carro Y sai com velocidade constante de 90 Km/h.
 
-#Em uma hora (60 minutos) o carro Y consegue se distanciar 30 quilômetros do carro X, ou seja, consegue se afastar um quilômetro a cada 2 minutos.
+Em uma hora (60 minutos) o carro Y consegue se distanciar 30 quilômetros do carro X, ou seja, consegue se afastar um quilômetro a cada 2 minutos.
 
-#Leia a distância (em Km) e calcule quanto tempo leva (em minutos) para o carro Y tomar essa distância do outro carro.
+Leia a distância (em Km) e calcule quanto tempo leva (em minutos) para o carro Y tomar essa distância do outro carro.
 
-#Entrada
-#O arquivo de entrada contém um número inteiro.
+Entrada
+O arquivo de entrada contém um número inteiro.
 
-#Saída
-#Imprima o tempo necessário seguido da mensagem "minutos".
+Saída
+Imprima o tempo necessário seguido da mensagem "minutos".'''
 
+def tempo_para_alcancar(distancia_km):
+    velocidade_x = 60
+    velocidade_y = 90
+    tempo = distancia_km / (velocidade_y - velocidade_x)
+    tempo_minutos = tempo * 60
+
+    return int(tempo_minutos)
+distancia_km = int(input())
+tempo_necessario = tempo_para_alcancar(distancia_km)
+print('{} minutos'.format(tempo_necessario))
